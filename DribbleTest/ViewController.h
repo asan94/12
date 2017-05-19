245b5f080705bf465aea47599a352d84ad04e0d1
biceps_one_love <ametov.asan.d@gmail.com>
2017-05-19 13:57:20 +0300

    Initial Commit

---
+++ DribbleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DribbleTest
+//
+//  Created by Аметов Асан on 19.05.17.
+//  Copyright © 2017 Asan Ametov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DribbleTestTests/DribbleTestTests.m
@@ -0,0 +1,39 @@
+//
+//  DribbleTestTests.m
+//  DribbleTestTests
+//
+//  Created by Аметов Асан on 19.05.17.
+//  Copyright © 2017 Asan Ametov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DribbleTestTests : XCTestCase
+
+@end
+
+@implementation DribbleTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
